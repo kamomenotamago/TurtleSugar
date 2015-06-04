d237cd41dd00493b481ae1c70e6944116178da7a
kagomenotamago <g13929mr@gm.tsuda.ac.jp>
2015-06-04 15:36:52 +0900

    super書き換え

--- src/jp/tuyano/PlaceServlet.java
+++ src/jp/tuyano/PlaceServlet.java
@@ -11,6 +11,7 @@ public class PlaceServlet extends HttpServlet {
 	    res.setContentType("text/html; charset=UTF-8");
 	    PrintWriter out = res.getWriter();
 	    //クライアントに送るHTML
+	    out.println("<div align=(center)");
 	    out.println("<HTML>");
 	    out.println("<HEAD>");
 	    out.println("<TITLE>配送場所・配送日時</TITLE>");
@@ -22,8 +23,9 @@ public class PlaceServlet extends HttpServlet {
 	    out.println("<tr>");
 	    out.println("<th>"+"<h1>配送場所</h1>"+req.getParameter("place"));
 	    out.println("<th>" + "<h1>配送日時</h1>"+ req.getParameter("date"));
-	    out.println("</tr>");
+	    out.println("</tr></div>");
 	    out.println("</table>");
+	    out.println("<a href=thankyou.html>この内容で注文する</a>");
 	    out.println("</BODY>");
 	    out.println("</HTML>");
 	    out.close();
